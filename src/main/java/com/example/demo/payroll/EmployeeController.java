85998454cf25c4f8a9b5edeb2551b45444fd07ca
estebanpisani <estebandpisani@gmail.com>
2025-06-26 01:12:52 -0300

    Spring HATEOAS

--- src/main/java/com/example/demo/payroll/EmployeeController.java
+++ src/main/java/com/example/demo/payroll/EmployeeController.java
@@ -1,8 +1,15 @@
 package com.example.demo.payroll;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -14,30 +21,48 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    public List<Employee> all() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = repository.findAll()
+                .stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
-    public Employee newEmployee(@RequestBody Employee newEmployee){
-        return repository.save(newEmployee);
+    public EntityModel<Employee> newEmployee(@RequestBody Employee newEmployee){
+        Employee savedEmployee = repository.save(newEmployee);
+        return EntityModel.of(savedEmployee,
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"),
+                linkTo(methodOn(EmployeeController.class).one(savedEmployee.getId())).withSelfRel());
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable Long id){
-        return repository.findById(id)
-                .orElseThrow(()-> new EmployeeNotFoundException(id));
+    public EntityModel<Employee> one(@PathVariable Long id){
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
-    public Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
-        return repository.findById(id)
-                .map(employee -> {
+    public EntityModel<Employee> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
+        Employee updatedEmployee = repository.findById(id).map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setRole(newEmployee.getRole());
                     return repository.save(employee);
                 })
                 .orElseGet(() -> repository.save(newEmployee));
+
+        return EntityModel.of(updatedEmployee,
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"),
+                linkTo(methodOn(EmployeeController.class).one(updatedEmployee.getId())).withSelfRel());
     }
 
     @DeleteMapping("/employees/{id}")
